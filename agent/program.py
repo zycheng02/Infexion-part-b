38519683e49d671709aede23d98bda0ce466344b
zhenming <zhenming.bi@student.unimelb.edu.au>
2023-05-03 21:22:07 +1000

    possible actions and calc heuristics

--- agent/program.py
+++ agent/program.py
@@ -2,7 +2,8 @@
 # Project Part B: Game Playing Agent
 
 from referee.game import \
-    PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
+    PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir, Board
+import copy
 
 
 # This is the entry point for your game playing agent. Currently the agent
@@ -11,7 +12,57 @@ from referee.game import \
 # intended to serve as an example of how to use the referee API -- obviously
 # this is not a valid strategy for actually playing the game!
 
+def fill_full_dict():
+        board_dict = {}
+        for i in range(0, 7):
+            for j in range(0, 7):
+                board_dict[HexPos(i, j)] = None
+        return board_dict
+
+
 class Agent:
+
+    def calc_heuristics(self, board: Board, action: Action):
+        temp_board = copy.deepcopy(board)
+        temp_board.apply_action(action)
+        oppo = [pos for pos in temp_board._state.keys() if temp_board._state[pos].player != self._color]
+        if len(oppo) == 1:
+            return 1
+        for op in oppo:
+            for dir in HexDir:
+                temp = op
+                for i in range(6):
+                    temp.__add__(dir)
+                    if temp in oppo:
+                        oppo.remove(temp)
+        temp_board.undo_action()
+        num_lines = len(oppo)
+        return num_lines
+                    
+
+    def possible_actions(self):
+        b_dict = self.board._state
+        spawn_dict = fill_full_dict()
+        spread_dict = []
+        for pos, state in b_dict.items():
+            del spawn_dict[pos]
+            if state.player == self._color:
+                print(state.player)
+                print(self._color)
+                spread_dict.append(pos)
+        
+        for i in spread_dict:
+            for dir in HexDir:
+                print(i)
+                print("-------------")
+                print(self.calc_heuristics(self.board, SpreadAction(i, dir)))
+                return SpreadAction(i, dir)
+        
+        for i in spawn_dict.keys():
+            print("-------------")
+            print(self.calc_heuristics(self.board, SpawnAction(i)))
+            return SpawnAction(i)
+
     def __init__(self, color: PlayerColor, **referee: dict):
         """
         Initialise the agent.
@@ -22,6 +73,7 @@ class Agent:
                 print("Testing: I am playing as red")
             case PlayerColor.BLUE:
                 print("Testing: I am playing as blue")
+        self.board = Board()
 
     def action(self, **referee: dict) -> Action:
         """
@@ -29,15 +81,23 @@ class Agent:
         """
         match self._color:
             case PlayerColor.RED:
+                if self.board._history != None:
+                    return self.possible_actions()
                 return SpawnAction(HexPos(3, 3))
             case PlayerColor.BLUE:
                 # This is going to be invalid... BLUE never spawned!
+                if self.board._history != None:
+                    return self.possible_actions()
+                return SpawnAction(HexPos(3, 4))
                 return SpreadAction(HexPos(3, 3), HexDir.Up)
 
     def turn(self, color: PlayerColor, action: Action, **referee: dict):
         """
         Update the agent with the last player's action.
         """
+        # if self._color != color:
+        print(self.board.__getitem__(HexPos(0,0)))
+        self.board.apply_action(action)
         match action:
             case SpawnAction(cell):
                 print(f"Testing: {color} SPAWN at {cell}")
