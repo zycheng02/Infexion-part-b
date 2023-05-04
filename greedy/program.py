f95e4929e228bdc661ea49f9f440afbead2e9bc4
zhenming <zhenming.bi@student.unimelb.edu.au>
2023-05-04 14:25:07 +1000

    implemented random and greedy agent

--- greedy/program.py
+++ greedy/program.py
@@ -3,7 +3,7 @@
 
 from referee.game import \
     PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir, Board
-import copy
+import copy, sys
 
 
 # This is the entry point for your game playing agent. Currently the agent
@@ -11,6 +11,8 @@ import copy
 # spreads a token at the centre of the board if playing as BLUE. This is
 # intended to serve as an example of how to use the referee API -- obviously
 # this is not a valid strategy for actually playing the game!
+
+
 
 def fill_full_dict():
         board_dict = {}
@@ -22,46 +24,74 @@ def fill_full_dict():
 
 class Agent:
 
-    def calc_heuristics(self, board: Board, action: Action):
-        temp_board = copy.deepcopy(board)
+    # def calc_heuristics(self, action: Action):
+    #     temp_board = copy.deepcopy(self.board)
+    #     temp_board.apply_action(action)
+    #     oppo = [pos for pos in temp_board._state.keys() if temp_board._state[pos].player != self._color]
+    #     if len(oppo) == 1:
+    #         return 1
+    #     for op in oppo:
+    #         for dir in HexDir:
+    #             temp = op
+    #             for i in range(6):
+    #                 temp.__add__(dir)
+    #                 if temp in oppo:
+    #                     oppo.remove(temp)
+    #     temp_board.undo_action()
+    #     num_lines = len(oppo)
+    #     return num_lines
+    
+    def calc_heuristics(self, action: Action):
+        temp_board = copy.deepcopy(self.board)
         temp_board.apply_action(action)
-        oppo = [pos for pos in temp_board._state.keys() if temp_board._state[pos].player != self._color]
-        if len(oppo) == 1:
-            return 1
-        for op in oppo:
-            for dir in HexDir:
-                temp = op
-                for i in range(6):
-                    temp.__add__(dir)
-                    if temp in oppo:
-                        oppo.remove(temp)
+        self_k = self_n = oppo_k = oppo_n = 0
+        for pos, state in temp_board._state.items():
+            if state.player == self._color:
+                self_n += 1
+                self_k += state.power
+            else:
+                oppo_n += 1
+                oppo_k += state.power
         temp_board.undo_action()
-        num_lines = len(oppo)
-        return num_lines
+        return (oppo_k ** oppo_n) / (self_k ** self_n)
                     
 
     def possible_actions(self):
         b_dict = self.board._state
         spawn_dict = fill_full_dict()
         spread_dict = []
+        action_list = []
         for pos, state in b_dict.items():
             del spawn_dict[pos]
             if state.player == self._color:
-                print(state.player)
-                print(self._color)
+                # print(state.player)
+                # print(self._color)
                 spread_dict.append(pos)
         
         for i in spread_dict:
             for dir in HexDir:
-                print(i)
-                print("-------------")
-                print(self.calc_heuristics(self.board, SpreadAction(i, dir)))
-                return SpreadAction(i, dir)
+                # print(i)
+                # print("-------------")
+                # print(self.calc_heuristics(SpreadAction(i, dir)))
+                action_list.append(SpreadAction(i, dir))
         
         for i in spawn_dict.keys():
-            print("-------------")
-            print(self.calc_heuristics(self.board, SpawnAction(i)))
-            return SpawnAction(i)
+            # print("-------------")
+            # print(self.calc_heuristics(SpawnAction(i)))
+            action_list.append(SpawnAction(i))
+
+        return action_list
+    
+    def greedy_select(self):
+        h = sys.maxsize
+        action_list = self.possible_actions()
+        final_action = action_list[0]
+        for action in action_list:
+            curr_h = self.calc_heuristics(action)
+            if curr_h < h:
+                h = curr_h
+                final_action = action
+        return final_action
 
     def __init__(self, color: PlayerColor, **referee: dict):
         """
@@ -82,12 +112,12 @@ class Agent:
         match self._color:
             case PlayerColor.RED:
                 if self.board._history != None:
-                    return self.possible_actions()
+                    return self.greedy_select()
                 return SpawnAction(HexPos(3, 3))
             case PlayerColor.BLUE:
                 # This is going to be invalid... BLUE never spawned!
                 if self.board._history != None:
-                    return self.possible_actions()
+                    return self.greedy_select()
                 return SpawnAction(HexPos(3, 4))
                 return SpreadAction(HexPos(3, 3), HexDir.Up)
 
@@ -96,7 +126,7 @@ class Agent:
         Update the agent with the last player's action.
         """
         # if self._color != color:
-        print(self.board.__getitem__(HexPos(0,0)))
+        # print(self.board.__getitem__(HexPos(0,0)))
         self.board.apply_action(action)
         match action:
             case SpawnAction(cell):
