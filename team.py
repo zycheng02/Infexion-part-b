40398f7f161b27f2efa5b19c4e7a13b9621d36a9
Jasmine <82012560+bzm-jas@users.noreply.github.com>
2023-05-10 22:49:25 +1000

    Add files via upload

---
+++ team.py
@@ -0,0 +1,30 @@
+# COMP30024 Artificial Intelligence, Semester 1 2023
+# Project Partner Nomination
+
+# Fill in your team details here. Make sure you always include this file in
+# future project submissions *unchanged* -- this allows us to keep track of who
+# is submitting what, and ensures credit is awarded correctly.
+
+metadata = {
+    # Choose a team name! Try to make it unique in some way, as it will be
+    # used to identify your team in the tournament at the end of the semester.
+    # - Allowed characters: A-Z, a-z, 0-9, -, _
+    # - Minimum length: 3 characters
+    # - Maximum length: 20 characters
+    "team_name": f"ChatGPT", 
+
+    # Fill in the student IDs and emails of both team members here. There must
+    # be exactly two members in the list. 
+    "team_members": [
+        {
+            "name": "Zhenming Bi",
+            "student_id": "1269131",
+            "email": "zhenming@student.unimelb.edu.au" # Must be @student.unimelb.edu.au address
+        },
+        {
+            "name": "Zhiyuan Cheng",
+            "student_id": "1271641",
+            "email": "zdcheng@student.unimelb.edu.au" # Must be @student.unimelb.edu.au address
+        }
+    ]
+}
